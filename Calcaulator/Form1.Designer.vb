d207d0106726281b0ec68eb1a65082d906c76f22
Deba700 <djana0284@gmail.com>
2025-06-23 23:57:09 +0530

    calculator project is created

---
+++ Calcaulator/Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Btn10 = New Button()
+        Btn11 = New Button()
+        Btn12 = New Button()
+        BtnPlus = New Button()
+        Btn1 = New Button()
+        Btn2 = New Button()
+        Btn3 = New Button()
+        BtnMinus = New Button()
+        Btn4 = New Button()
+        Btn5 = New Button()
+        Btn6 = New Button()
+        BtnMultiplication = New Button()
+        Btn7 = New Button()
+        Btn8 = New Button()
+        Btn9 = New Button()
+        BtnDevide = New Button()
+        Btn17 = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Btn10
+        ' 
+        Btn10.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn10.Location = New Point(154, 325)
+        Btn10.Name = "Btn10"
+        Btn10.Size = New Size(67, 68)
+        Btn10.TabIndex = 0
+        Btn10.Text = "0"
+        Btn10.UseVisualStyleBackColor = True
+        ' 
+        ' Btn11
+        ' 
+        Btn11.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn11.Location = New Point(227, 325)
+        Btn11.Name = "Btn11"
+        Btn11.Size = New Size(67, 68)
+        Btn11.TabIndex = 0
+        Btn11.Text = "."
+        Btn11.UseVisualStyleBackColor = True
+        ' 
+        ' Btn12
+        ' 
+        Btn12.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn12.Location = New Point(300, 325)
+        Btn12.Name = "Btn12"
+        Btn12.Size = New Size(67, 68)
+        Btn12.TabIndex = 0
+        Btn12.Text = "C"
+        Btn12.UseVisualStyleBackColor = True
+        ' 
+        ' BtnPlus
+        ' 
+        BtnPlus.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BtnPlus.Location = New Point(391, 325)
+        BtnPlus.Name = "BtnPlus"
+        BtnPlus.Size = New Size(67, 68)
+        BtnPlus.TabIndex = 0
+        BtnPlus.Text = "+"
+        BtnPlus.UseVisualStyleBackColor = True
+        ' 
+        ' Btn1
+        ' 
+        Btn1.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn1.Location = New Point(154, 251)
+        Btn1.Name = "Btn1"
+        Btn1.Size = New Size(67, 68)
+        Btn1.TabIndex = 0
+        Btn1.Text = "1"
+        Btn1.UseVisualStyleBackColor = True
+        ' 
+        ' Btn2
+        ' 
+        Btn2.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn2.Location = New Point(227, 251)
+        Btn2.Name = "Btn2"
+        Btn2.Size = New Size(67, 68)
+        Btn2.TabIndex = 0
+        Btn2.Text = "2"
+        Btn2.UseVisualStyleBackColor = True
+        ' 
+        ' Btn3
+        ' 
+        Btn3.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn3.Location = New Point(300, 251)
+        Btn3.Name = "Btn3"
+        Btn3.Size = New Size(67, 68)
+        Btn3.TabIndex = 0
+        Btn3.Text = "3"
+        Btn3.UseVisualStyleBackColor = True
+        ' 
+        ' BtnMinus
+        ' 
+        BtnMinus.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BtnMinus.Location = New Point(391, 251)
+        BtnMinus.Name = "BtnMinus"
+        BtnMinus.Size = New Size(67, 68)
+        BtnMinus.TabIndex = 0
+        BtnMinus.Text = "-"
+        BtnMinus.UseVisualStyleBackColor = True
+        ' 
+        ' Btn4
+        ' 
+        Btn4.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn4.Location = New Point(154, 177)
+        Btn4.Name = "Btn4"
+        Btn4.Size = New Size(67, 68)
+        Btn4.TabIndex = 0
+        Btn4.Text = "4"
+        Btn4.UseVisualStyleBackColor = True
+        ' 
+        ' Btn5
+        ' 
+        Btn5.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn5.Location = New Point(227, 177)
+        Btn5.Name = "Btn5"
+        Btn5.Size = New Size(67, 68)
+        Btn5.TabIndex = 0
+        Btn5.Text = "5"
+        Btn5.UseVisualStyleBackColor = True
+        ' 
+        ' Btn6
+        ' 
+        Btn6.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn6.Location = New Point(300, 177)
+        Btn6.Name = "Btn6"
+        Btn6.Size = New Size(67, 68)
+        Btn6.TabIndex = 0
+        Btn6.Text = "6"
+        Btn6.UseVisualStyleBackColor = True
+        ' 
+        ' BtnMultiplication
+        ' 
+        BtnMultiplication.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BtnMultiplication.Location = New Point(391, 177)
+        BtnMultiplication.Name = "BtnMultiplication"
+        BtnMultiplication.Size = New Size(67, 68)
+        BtnMultiplication.TabIndex = 0
+        BtnMultiplication.Text = "*"
+        BtnMultiplication.UseVisualStyleBackColor = True
+        ' 
+        ' Btn7
+        ' 
+        Btn7.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn7.Location = New Point(154, 103)
+        Btn7.Name = "Btn7"
+        Btn7.Size = New Size(67, 68)
+        Btn7.TabIndex = 0
+        Btn7.Text = "7"
+        Btn7.UseVisualStyleBackColor = True
+        ' 
+        ' Btn8
+        ' 
+        Btn8.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn8.Location = New Point(227, 103)
+        Btn8.Name = "Btn8"
+        Btn8.Size = New Size(67, 68)
+        Btn8.TabIndex = 0
+        Btn8.Text = "8"
+        Btn8.UseVisualStyleBackColor = True
+        ' 
+        ' Btn9
+        ' 
+        Btn9.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn9.Location = New Point(300, 103)
+        Btn9.Name = "Btn9"
+        Btn9.Size = New Size(67, 68)
+        Btn9.TabIndex = 0
+        Btn9.Text = "9"
+        Btn9.UseVisualStyleBackColor = True
+        ' 
+        ' BtnDevide
+        ' 
+        BtnDevide.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BtnDevide.Location = New Point(391, 103)
+        BtnDevide.Name = "BtnDevide"
+        BtnDevide.Size = New Size(67, 68)
+        BtnDevide.TabIndex = 0
+        BtnDevide.Text = "/"
+        BtnDevide.UseVisualStyleBackColor = True
+        ' 
+        ' Btn17
+        ' 
+        Btn17.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn17.Location = New Point(154, 416)
+        Btn17.Name = "Btn17"
+        Btn17.Size = New Size(304, 65)
+        Btn17.TabIndex = 1
+        Btn17.Text = "="
+        Btn17.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(148, 12)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(310, 80)
+        TextBox1.TabIndex = 2
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(652, 493)
+        Controls.Add(TextBox1)
+        Controls.Add(Btn17)
+        Controls.Add(BtnDevide)
+        Controls.Add(BtnMultiplication)
+        Controls.Add(BtnMinus)
+        Controls.Add(BtnPlus)
+        Controls.Add(Btn9)
+        Controls.Add(Btn6)
+        Controls.Add(Btn3)
+        Controls.Add(Btn12)
+        Controls.Add(Btn8)
+        Controls.Add(Btn7)
+        Controls.Add(Btn5)
+        Controls.Add(Btn4)
+        Controls.Add(Btn2)
+        Controls.Add(Btn1)
+        Controls.Add(Btn11)
+        Controls.Add(Btn10)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Btn10 As Button
+    Friend WithEvents Btn11 As Button
+    Friend WithEvents Btn12 As Button
+    Friend WithEvents BtnPlus As Button
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Btn2 As Button
+    Friend WithEvents Btn3 As Button
+    Friend WithEvents BtnMinus As Button
+    Friend WithEvents Btn4 As Button
+    Friend WithEvents Btn5 As Button
+    Friend WithEvents Btn6 As Button
+    Friend WithEvents BtnMultiplication As Button
+    Friend WithEvents Btn7 As Button
+    Friend WithEvents Btn8 As Button
+    Friend WithEvents Btn9 As Button
+    Friend WithEvents BtnDevide As Button
+    Friend WithEvents Btn17 As Button
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Calcaulator/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Private Sub Number_Click(sender As Object, e As EventArgs) Handles Btn1.Click, Btn2.Click, Btn3.Click, Btn4.Click, Btn5.Click, Btn6.Click, Btn7.Click, Btn8.Click, Btn9.Click
+
+
+        Dim BtnValue As Button = CType(sender, Button)
+        TextBox1.Text &= BtnValue.Text
+
+
+    End Sub
+
+    Private Sub Btn10_Zero(sender As Object, e As EventArgs) Handles Btn10.Click
+
+        If TextBox1.Text = String.Empty Then
+
+        Else
+            TextBox1.Text &= "0"
+        End If
+    End Sub
+
+    Private Sub Btn11_Decimal(sender As Object, e As EventArgs) Handles Btn11.Click
+        If TextBox1.Text = String.Empty Then
+            TextBox1.Text &= "0."
+        ElseIf Not TextBox1.Text.Contains(".") Then
+            TextBox1.Text &= "."
+        End If
+    End Sub
+
+    Private Sub Btn12_Clear(sender As Object, e As EventArgs) Handles Btn12.Click
+        TextBox1.Text = String.Empty
+
+    End Sub
+
+    Private Sub Operators(sender As Object, e As EventArgs) Handles BtnPlus.Click, BtnMinus.Click, BtnMultiplication.Click, BtnDevide.Click
+        TextBox1.Text &= CType(sender, Button).Text
+
+    End Sub
+
+    Private Sub EqualsToClick(sender As Object, e As EventArgs) Handles Btn17.Click
+        Try
+            Dim exprassion As String = TextBox1.Text
+            Dim result As Object
+            Dim table As New DataTable
+            result = table.Compute(exprassion, String.Empty)
+            TextBox1.Text = result.ToString
+        Catch ex As Exception
+            TextBox1.Text = "Eroor"
+        End Try
+
+
+    End Sub
+End Class
